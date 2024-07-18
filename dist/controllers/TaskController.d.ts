560dbfcc790c6314957bafe3cd04a4590347a5e5
Christian Adrian Lerma Baquero <christianlerma@gmail.com>
2024-07-18 17:54:00 -0500

    deploy test

--- dist/controllers/TaskController.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response } from 'express';
-export declare class TaskController {
-    static createTask: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-    static getProjectTasks: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-    static getTasksById: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-    static updateTask: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-    static deleteTask: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-    static updateStatus: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-}
--- dist/utils/auth.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const hassPassword: (password: string) => Promise<string>;
-export declare const checkPassword: (enteregPassword: string, storeHash: string) => Promise<boolean>;
